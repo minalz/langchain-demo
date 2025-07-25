a8ebf078f37126d76ce7eb34fc6064d327be4c03
minalz-pc <119687281@qq.com>
2025-07-26 01:51:49 +0800

    feat: update

--- my-app/app/server.py
+++ my-app/app/server.py
@@ -1,13 +1,15 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from langserve import add_routes
 from langchain_openai import ChatOpenAI
-import os
-from dotenv import load_dotenv
+from langserve import add_routes
+from starlette.responses import RedirectResponse
 
-load_dotenv()
-
-app = FastAPI()
+app = FastAPI(
+    title="LangChain 服务器",
+    version="1.0",
+    description="使用 Langchain 的 Runnable 接口的简单 API 服务器",)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,16 +22,25 @@ llm = ChatOpenAI(
     model="qwen-plus",
     openai_api_key=os.getenv("DASHSCOPE_API_KEY"),
     openai_api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    # openai_api_base="https://dashscope.aliyuncs.com/api/v1",
     temperature=0.7
 )
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+
 prompt = ChatPromptTemplate.from_messages([("human", "{question}")])
 chain = prompt | llm | StrOutputParser()
 
 add_routes(app, chain, path="/chat")
 
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8011)
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
